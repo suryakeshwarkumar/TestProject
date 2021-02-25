005421413ad636f3d4d771dfa6da3b328eccb7ef
Suryakeshwar Kumar <Suryakeshwar Kumar@LAPTOP-IO3FORF4>
2021-02-25 12:57:46 +0530

    first commit

---
+++ testingProj/src/com/app/surya/BeanClass.java
@@ -0,0 +1,13 @@
+package com.app.surya;
+
+public class BeanClass {
+	
+	private int sid;
+	private String sname;
+	@Override
+	public String toString() {
+		return "BeanClass [sid=" + sid + ", sname=" + sname + "]";
+	}
+	
+
+}
